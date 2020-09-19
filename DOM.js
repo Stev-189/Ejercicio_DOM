e3e8ff9618945eea98b3953fac594b4312711b94
Stev-189 <epavezllanca@gmail.com>
2020-09-18 22:32:18 -0300

    funciones completas

--- DOM.js
+++ DOM.js
@@ -1,7 +1,15 @@
 const $menuNav= document.getElementById("menuPalanca"),
       $hamburger=document.querySelector(".hamburger"),
-      palancaMenu=()=>$menuNav.classList.toggle("palanca"),
-      hamburgerAnimated=()=>$hamburger.classList.toggle("is-active")
+      $reloj_container=document.getElementById("reloj_container"),
+      $button_view_reloj=document.getElementById("button-view-reloj"),
+      $button_sound_inicia=document.getElementById("button-sound-inicia"),
+      palancaMenu=_=>$menuNav.classList.toggle("palanca"),
+      hamburgerAnimated=_=>$hamburger.classList.toggle("is-active"),
+      mueveReloj=_=>{if(continuar)document.form_reloj.reloj.value=new Date().toLocaleTimeString(),setTimeout("mueveReloj()",1000)},
+      play=_=>{if(continuar_play){new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3').play(),setTimeout("play()",1000)}}
+
+let continuar,
+    continuar_play
 
 document.addEventListener("click",(e)=>{
   if(e.target.matches(".botonPalanca")||e.target.matches(".hamburger")||e.target.matches(".hamburger-inner")||e.target.matches(".hamburger-box")){
@@ -9,16 +17,28 @@ document.addEventListener("click",(e)=>{
     hamburgerAnimated(e)
     e.stopPropagation()
   }
+  if(e.target.matches("#button-view-reloj")){
+    continuar=true
+    mueveReloj()
+    $reloj_container.style.setProperty("display","flex")
+    $button_view_reloj.setAttribute("disabled","true")
+    e.stopPropagation()
+  }
+  if(e.target.matches("#button-view-stop")){
+    continuar=false
+    $reloj_container.style.setProperty("display","none")
+    $button_view_reloj.removeAttribute("disabled")
+    e.stopPropagation()
+  }
+  if(e.target.matches("#button-sound-inicia")){
+    continuar_play=true
+    play()
+    $button_sound_inicia.setAttribute("disabled","true")
+    e.stopPropagation()
+  }
+  if(e.target.matches("#button-sound-stop")){
+    continuar_play=false
+    $button_sound_inicia.removeAttribute("disabled")
+    e.stopPropagation()
+  }
 })
-
-const $reloj=document.getElementById("reloj"),
-      $button_view_reloj=document.getElementById("button-view-reloj"),
-      $button_view_stop=document.getElementById("button-view-stop"),
-      $button_sound_inicia=document.getElementById("button-sound-inicia"),
-      $button_sound_stop=document.getElementById("button-sound-stop"),
-      $reloj_creator=document.createTextNode(`${new Date().toLocaleTimeString()}`)
-
-      setInterval(() => {
-        $reloj.appendChild($reloj_creator)
-      }, 1000);
-      
