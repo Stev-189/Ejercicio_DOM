4715725633a7f5488303eb555583c3c1e43b83a4
Stev-189 <epavezllanca@gmail.com>
2020-09-24 08:01:32 -0300

    Aplicnado Dark-Mode diseño con LocalStorage

--- DOM.js
+++ DOM.js
@@ -143,19 +143,31 @@ document.addEventListener("click",(e)=>{
   }
 })
 /////////////////////////////////////////////////////////////////////////////////// Dark Mode
-const $dark_mode = document.getElementById("dark-mode-container")
+let lsDarkMode=localStorage.getItem('dark-mode')
+const $dark_mode = document.getElementById("dark-mode-container"),
+      darkModeActive=_=>{
+        $dark_mode.innerHTML=`<button id="dark-mode-active" class="dark-mode-btn">🌔</button>`
+        document.documentElement.style.setProperty('--first-color','#323330')
+        document.documentElement.style.setProperty('--second-color','240,219,79')
+        document.documentElement.style.setProperty('--b-B-Color','#616061')
+        localStorage.setItem('dark-mode',true)
+      },
+      darkModeDesActive=_=>{
+        $dark_mode.innerHTML=`<button id="dark-mode" class="dark-mode-btn">🌒</button>`
+        document.documentElement.style.setProperty('--first-color','#f0db4f')
+        document.documentElement.style.setProperty('--second-color','50,51,48')
+        document.documentElement.style.setProperty('--b-B-Color','white')
+        localStorage.removeItem('dark-mode')
+      },
+      inLocalStorageDarkMode=_=>{
+        if(lsDarkMode) darkModeActive()
+      }
+window.onload=inLocalStorageDarkMode()
 document.addEventListener("click",(e)=>{
   if(e.target.matches("#dark-mode")){
-    $dark_mode.innerHTML=`<button id="dark-mode-active" class="dark-mode-btn">🌔</button>`
-    document.documentElement.style.setProperty('--first-color','#323330')
-    document.documentElement.style.setProperty('--second-color','240,219,79')
-    document.documentElement.style.setProperty('--b-B-Color','#616061')
+    darkModeActive()
   }
   if(e.target.matches("#dark-mode-active")){
-    $dark_mode.innerHTML=`<button id="dark-mode" class="dark-mode-btn">🌒</button>`
-    document.documentElement.style.setProperty('--first-color','#f0db4f')
-    document.documentElement.style.setProperty('--second-color','50,51,48')
-    document.documentElement.style.setProperty('--b-B-Color','white')
-
+    darkModeDesActive()
   }
 })
