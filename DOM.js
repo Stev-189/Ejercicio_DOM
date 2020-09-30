e4cb06bf021d8e9d3fca193b32510a157ea52bc1
Stev-189 <epavezllanca@gmail.com>
2020-09-30 09:42:37 -0300

    S09V1 Filtrar

--- DOM.js
+++ DOM.js
@@ -396,3 +396,47 @@ function ViewError(err){alert(err)}
 document.addEventListener('click',e=>{
   if(e.target.matches('#btnGeo'))loadLocation('btnGeo')
 })
+///////////////////////////////////////////////////////////////////////////////////Section 9 Buscar
+let arrayApi=['Tech','Animals','People','Arch','Nature','Animals','People','Arch','Nature'],
+   $intBuscar=document,
+   comp=false,
+   $contDiv
+
+const addElement=(e,arr)=>{
+  let container=document.getElementById(e)
+  arr.forEach((e,n)=>{ let {titulo, link}=e
+    container.insertAdjacentHTML('beforeend',
+      ` <figure class="card ${titulo.toLocaleLowerCase()}">
+          <img src="${link}" alt="${titulo}">
+          <figcaption>${titulo}</figcaption>
+        </figure>` 
+    )})}
+
+function simularApi(e){
+  if(!document.getElementById(`${e}Div`)){
+    let rAApi=[]
+    const $conEl=document.getElementById(e)
+    arrayApi.forEach(e=>rAApi.push({titulo:e,link:`https://placeimg.com/200/200/${e.toLocaleLowerCase()}`}))
+    $conEl.insertAdjacentHTML('afterend',`<div id=${e}Div></div>`)
+    $conEl.insertAdjacentHTML('afterend',`<input type="text" id=${e}Input placeholder='Ingrese elemento a buscar' >`)
+    comp=`${e}Input`
+    $intBuscar=document.getElementById(`${e}Input`)
+    $contDiv=document.getElementById(`${e}Div`)
+    addElement(`${e}Div`,rAApi)
+  }}
+  
+$intBuscar.addEventListener("keyup",(e)=>{
+  let $class=$contDiv.querySelectorAll('.card'),
+  regex=RegExp(e.target.value,'i')
+  if(comp===e.target.id){
+    if(e.target.value.length===0){
+      $class.forEach(e=>e.classList.remove('fHidden'))
+    } else{
+      $class.forEach(e=>regex.test(e.classList[1])
+        ?e.classList.remove('fHidden')
+        :e.classList.add('fHidden'))
+    }}})
+
+document.addEventListener('click',e=>{
+  if(e.target.matches('#btnIFiltro'))simularApi(e.target.id)
+})
